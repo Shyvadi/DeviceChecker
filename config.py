8c1bb4ff01661ed772acf66fc96dc7c3c6e09825
AtakuHydra <sfsuper2@gmail.com>
2018-12-10 01:28:52 -0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,18 @@
+# This is the config file for the bot
+
+
+
+Phone_uuids = ["",""]
+# Add phone uuids like: ["phoneuuid1", "phoneuuid2"] etc etc
+
+TOKEN = '' # Token for the Bot
+
+YourDiscordID = ''  # The ID of YOUR Discord Account (Allows only you to start the bot)
+
+user=''
+password=''
+host=''
+port=''
+database=''
+
+query = "SELECT uuid, last_seen,instance_name FROM DATABASENAME.device"
